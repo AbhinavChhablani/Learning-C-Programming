43e0448ca026234cc7e49abe3f4208eaec64e357
Abhinav Chhablani <chhablaniabhinav@gmail.com>
2026-04-28 20:56:30 +0530

    Delete Daybydaynumber.c to put in folder

--- Daybydaynumber.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main(){
-    int n;
-    char day[7][10] ={"Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"};
-    printf("Enter The Day Number :");
-    scanf("%d",&n);
-    printf("The Day Is %s .",day[n-1]);
-    return 0;
-}
